8937a94f37995eb840373bc828d61311980c9212
shlee89-lunapapa <cutyzombie@gmail.com>
2026-06-01 11:11:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+// 분당승무 Service Worker
+const CACHE_NAME = 'bundang-v1';
+const CACHE_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CACHE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // GAS API 요청은 캐시 안함 (항상 네트워크)
+  if (e.request.url.includes('script.google.com')) {
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
